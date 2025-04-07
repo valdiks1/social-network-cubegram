ac24eec27b48f4376d3bc7be8ee5e6fdb3e6065a
Vladyslav Pehushyn <vlad.pegushyn1@gmail.com>
2025-04-07 22:49:35 +0200

    fix design navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,33 +2,45 @@ import { Link, NavLink } from "react-router";
 import SearchI from "../../assets/Search.svg";
 import LogInModal from "../LogInModal/LogInModal";
 import "./Navbar.css";
-import {useState} from 'react';
+import { useState } from 'react';
+import avatar from '../../assets/images/avatar.svg';
 
 const Navbar = (props) => {
 
     const [modalState, setModalState] = useState(false);
 
-    return(<>
+    return (<>
         <header>
             <nav>
                 <div className="navbar-inner">
-                    <div className="logo">
-                        <Link to='/' className="logoLink" href="">Cubegram</Link>
+                    <div className="left">
+                        <div className="logo">
+                            <Link to='/' className="logoLink" href="">Cubegram</Link>
+                        </div>
+                        <ul className="nav-links">
+                            <li><NavLink to='/' className="nav-link" href="">Main</NavLink></li>
+                            <li><NavLink to='/records' className="nav-link" href="">Records</NavLink></li>
+                            <li><NavLink to='/rooms' className="nav-link" href="">Rooms</NavLink></li>
+                        </ul>
                     </div>
-                    <ul className="nav-links">
-                        <li><NavLink to='/' className="nav-link" href="">Main</NavLink></li>
-                        <li><NavLink to='/records'className="nav-link" href="">Records</NavLink></li>
-                        <li><NavLink to='/rooms' className="nav-link" href="">Rooms</NavLink></li>
-                    </ul>
+
                     <div className="right">
-                        <a href=""><img className="searchI" src={SearchI}/></a>
-                        <button onClick={() => setModalState(true)} className="login-btn">Log in</button>
+                        <a href=""><img className="searchI" src={SearchI} /></a>
+
+                        {props.authStatus ? (<button onClick={() => setModalState(true)} className="login-btn">Log in</button>) : (
+                            <div className="right-inner">
+                                <button className="logout-btn">Log out</button>
+                                <div className="profile-btn">
+                                    <NavLink className='profile-btn-link' to='/myprofile'><img src={avatar} alt="avatar" />My Profile</NavLink>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </nav>
         </header>
         <LogInModal authStatus={props.authStatus} setAuthStatus={props.setAuthStatus} call={modalState} onDestroy={() => setModalState(false)} />
-        </>
+    </>
     )
 }
 
