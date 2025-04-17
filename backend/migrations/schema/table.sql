2b91ba94fbe4e6571be9576e93c99fbfaf2e90bb
Vladyslav Pehushyn <vlad.pegushyn1@gmail.com>
2025-04-17 23:39:36 +0200

    add puzzles and attempts tables to schema

--- backend/migrations/schema/table.sql
+++ backend/migrations/schema/table.sql
@@ -27,3 +27,29 @@ CREATE TABLE IF NOT EXISTS public.users
     CONSTRAINT unique_login UNIQUE (login),
     CONSTRAINT check_sex_valid CHECK (sex::text = ANY (ARRAY['male'::character varying, 'female'::character varying]::text[]))
 )
+
+CREATE TABLE IF NOT EXISTS puzzles(
+	id serial NOT NULL,
+	name varchar(30) NOT NULL,
+	suspicious_result bigint NOT NULL, 	--milliseconds
+	CONSTRAINT puzzles_pkey PRIMARY KEY(id)
+);
+
+INSERT INTO puzzles(name, suspicious_result) 
+VALUES ('2x2x2', 200),
+('3x3x3', 2000),
+('4x4x4', 12000),
+('5x5x5', 28000);
+
+CREATE TABLE IF NOT EXISTS attempts(
+	id serial NOT NULL,
+	time bigint NOT NULL,											--result time
+	id_u int NOT NULL,
+	datetime timestamp without time zone NOT NULL,					--date of attempt
+	flags varchar(5) NOT NULL,
+	id_p int NOT NULL,
+	CONSTRAINT attempts_pkey PRIMARY KEY(id),
+	CONSTRAINT id_u_fk FOREIGN KEY(id_u) REFERENCES users(id),
+	CONSTRAINT id_p_fk FOREIGN KEY(id_p) REFERENCES puzzles(id),
+	CONSTRAINT flags_check CHECK (flags IN ('+2', 'DNF', 'none'))
+);
