2b9ab22af0b50eeeb7cb033c81eb4a6043e595c4
Vladyslav Pehushyn <vlad.pegushyn1@gmail.com>
2025-05-06 12:21:57 +0200

    fix: add thead and tbody tags to tables in RecordsComponent

--- frontend/src/components/RecordsComponent/RecordsComponent.jsx
+++ frontend/src/components/RecordsComponent/RecordsComponent.jsx
@@ -2,49 +2,65 @@ import './RecordsComponent.css';
 import Record from './Record';
 import { formatTime } from '../../utils/time';
 
-const RecordsComponent = ({records}) => {
+const RecordsComponent = ({ records }) => {
     return (
         <section className="records">
             <div className="card">
                 <h2>2x2x2</h2>
                 <table>
-                    <tr>
-                        <th>#</th>
-                        <th>Name Surname</th>
-                        <th>Country</th>
-                        <th>Record</th>
-                    </tr>
-                    {records.cube2.map((record, index) => <Record key={index} id={index+1} name={record.username} country={record.country} record={formatTime(record.record)}/>)}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name Surname</th>
+                            <th>Country</th>
+                            <th>Record</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {records.cube2.map((record, index) => <Record key={index} id={index + 1} name={record.username} country={record.country} record={formatTime(record.record)} />)}
+                    </tbody>
                 </table>
                 <h2>3x3x3</h2>
                 <table>
-                    <tr>
-                        <th>#</th>
-                        <th>Name Surname</th>
-                        <th>Country</th>
-                        <th>Record</th>
-                    </tr>
-                    {records.cube3.map((record, index) => <Record key={index} id={index+1} name={record.username} country={record.country} record={formatTime(record.record)}/>)}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name Surname</th>
+                            <th>Country</th>
+                            <th>Record</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {records.cube3.map((record, index) => <Record key={index} id={index + 1} name={record.username} country={record.country} record={formatTime(record.record)} />)}
+                    </tbody>
                 </table>
                 <h2>4x4x4</h2>
                 <table>
-                    <tr>
-                        <th>#</th>
-                        <th>Name Surname</th>
-                        <th>Country</th>
-                        <th>Record</th>
-                    </tr>
-                    {records.cube4.map((record, index) => <Record key={index} id={index+1} name={record.username} country={record.country} record={formatTime(record.record)}/>)}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name Surname</th>
+                            <th>Country</th>
+                            <th>Record</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {records.cube4.map((record, index) => <Record key={index} id={index + 1} name={record.username} country={record.country} record={formatTime(record.record)} />)}
+                    </tbody>
                 </table>
                 <h2>5x5x5</h2>
                 <table>
-                    <tr>
-                        <th>#</th>
-                        <th>Name Surname</th>
-                        <th>Country</th>
-                        <th>Record</th>
-                    </tr>
-                    {records.cube5.map((record, index) => <Record key={index} id={index+1} name={record.username} country={record.country} record={formatTime(record.record)}/>)}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name Surname</th>
+                            <th>Country</th>
+                            <th>Record</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {records.cube5.map((record, index) => <Record key={index} id={index + 1} name={record.username} country={record.country} record={formatTime(record.record)} />)}
+                    </tbody>
                 </table>
             </div>
         </section>
